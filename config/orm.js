821049ac71de9b6056412815c6e4229a1296d0a4
Miito <dinhlol69@gmail.com>
2020-05-11 14:19:17 -0400

    Implement a orm

---
+++ config/orm.js
@@ -0,0 +1,34 @@
+const connection = require('./connection');
+
+const orm = {
+  async selectAll (tableName) {
+    const query = 'SELECT * FROM ??;';
+    try {
+      const [rows] = await connection.query(query, tableName);
+      return rows;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async insertOne(tableName, column, value) {
+    const query = 'INSERT INTO ?? (??) VALUES (?);';
+    try {
+      const [rows] = await connection.query(query, [tableName, column, value]);
+      return rows
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async updateOne(tableName, column, columnValue, field, fieldValue) {
+    const query = 'UPDATE ?? SET ??=? WHERE ??=?;';
+    try {
+      const [rows] = await connection.query(query, [tableName, column, columnValue, field, fieldValue]);
+      return rows
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = orm;
+
